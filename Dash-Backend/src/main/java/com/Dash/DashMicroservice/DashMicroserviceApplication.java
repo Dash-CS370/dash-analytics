6c1206af6df15f4c55549f00ca43c7695fa22634
Vera Wen <verahui1@outlook.com>
2024-02-19 18:27:41 -0500

    Initial repo

---
+++ Dash-Backend/src/main/java/com/Dash/DashMicroservice/DashMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.Dash.DashMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Dash-Backend/src/test/java/com/Dash/DashMicroservice/DashMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dash.DashMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
