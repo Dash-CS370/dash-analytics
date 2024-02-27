caacf69018b3a5012541353025d7761ed98f5adf
Justin Lurie <jlurie02@gmail.com>
2024-02-27 15:15:15 -0500

    started dashboard

---
+++ dash-frontend/src/components/pages/dashboard/Sidebar/ProjectListItem.tsx
@@ -0,0 +1,68 @@
+import styles from '@/components/pages/dashboard/Sidebar/ProjectListItem.module.css';
+import { FC, useState, KeyboardEvent } from 'react';
+import { FiCheck, FiEdit2, FiTrash2 } from 'react-icons/fi';
+
+interface ProjectListItemProps {
+    id: number;
+    name: string;
+    onNameChange: (id: number, newName: string) => void;
+}
+
+export const ProjectListItem: FC<ProjectListItemProps> = ({
+    id,
+    name,
+    onNameChange,
+}) => {
+    // TODO: Add onDelete interaction (same as edit)
+    const [isEditing, setIsEditing] = useState(false);
+    const [newName, setNewName] = useState(name);
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleCheckClick = () => {
+        // update project name (onNameChange handles database update)
+        onNameChange(id, newName);
+        setIsEditing(false);
+    };
+
+    const handleEnterForProjName = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            handleCheckClick();
+        }
+    };
+
+    return (
+        <div className={styles.item}>
+            {isEditing ? (
+                <div className={styles.editContainer}>
+                    <input
+                        type="text"
+                        value={newName}
+                        onKeyDown={handleEnterForProjName}
+                        onChange={(e) => setNewName(e.target.value)}
+                        className={styles.editInput}
+                    />
+                    <FiCheck
+                        className={styles.icon}
+                        onClick={handleCheckClick}
+                    />
+                </div>
+            ) : (
+                <>
+                    <span className={styles.name}>{newName}</span>
+                    <div className={styles.actions}>
+                        <FiEdit2
+                            className={styles.icon}
+                            onClick={handleEditClick}
+                        />
+                        <FiTrash2
+                            className={styles.icon} /* onClick={onDelete} */
+                        />
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
