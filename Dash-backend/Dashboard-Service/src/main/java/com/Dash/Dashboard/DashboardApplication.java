99467475cbf66d334b8068a12723c670a6dab4f4
gpmorales <georgepm20002@gmail.com>
2024-02-27 03:03:38 -0500

    Parent module configured with base microservices

---
+++ Dash-backend/Dashboard-Service/src/main/java/com/Dash/Dashboard/DashboardApplication.java
@@ -0,0 +1,14 @@
+package com.Dash.Dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+}
