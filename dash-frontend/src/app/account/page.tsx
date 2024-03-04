07e16f0bb97976582de9604f5db364f0844735f7
LoveLoacker <khou6@emory.edu>
2024-03-04 18:59:05 -0500

    updates

---
+++ dash-frontend/src/app/account/page.tsx
@@ -0,0 +1,17 @@
+import styles from '@/app/account/page.module.css';
+import { NavBar } from '@/components/NavBar';
+
+export default function Account() {
+    return (
+        <main className={styles.main}>
+            <NavBar connected={true} />
+
+            <div className={styles.content}>
+                <h1 className={styles.title}>
+                    Account
+                </h1>
+                <div className={styles.horizontalLine}></div>
+            </div>
+        </main>
+    );
+}
