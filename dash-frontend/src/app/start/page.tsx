e20cc2a965922447323563edee5149569fdc8e3a
Jason Lebov <jason.lebov@emory.edu>
2024-03-05 14:13:55 -0500

    working start page

--- dash-frontend/src/app/start/page.tsx
+++ dash-frontend/src/app/start/page.tsx
@@ -1,23 +1,27 @@
-import styles from '@/app/dashboard/page.module.css';
-import { NavBar } from '@/components/NavBar';
+import styles from '@/app/start/page.module.css';
 import { PrimaryButton } from '@/components/buttons/PrimaryButton/PrimaryButton';
+import { SecondaryButton } from '@/components/buttons/SecondaryButton/SecondaryButton';
 
 export default function Start() {
     return (
         <main className={styles.main}>
-            <h1>Dash Analytics</h1>
-
             <div className={styles.content}>
-                <PrimaryButton href="/login" width="200px" height="60px">
-                    Login
-                </PrimaryButton>
-                <PrimaryButton
-                    href="/request-access"
-                    width="200px"
-                    height="60px"
-                >
-                    Request Access
-                </PrimaryButton>
+                <div className={styles.titleContainer}>
+                    <span className={styles.dash}>Dash</span>
+                    <span className={styles.analytics}>Analytics</span>
+                </div>
+                <div className={styles.buttons}>
+                    <PrimaryButton href="/login" width="350px" height="60px">
+                        Login
+                    </PrimaryButton>
+                    <SecondaryButton
+                        href="/request-access"
+                        width="350px"
+                        height="60px"
+                    >
+                        Request Access
+                    </SecondaryButton>
+                </div>
             </div>
         </main>
     );
