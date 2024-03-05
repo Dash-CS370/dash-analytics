bd233324d633b22b268c7c87542bb219b95d9576
LoveLoacker <khou6@emory.edu>
2024-03-04 19:22:57 -0500

    setup account page

---
+++ dash-frontend/src/components/pages/accountPage/AccountDetails.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { FC } from "react";
+import styles from "@/components/pages/accountPage/AccountPage.module.css";
+
+export const AccountDetails: FC = () => {
+    return(
+        <div className={styles.accountDetailsContainer}>
+            {/* TODO: implement */}
+        </div>
+    )
+}
---
+++ dash-frontend/src/components/pages/accountPage/CreditsSection.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { FC } from "react";
+
+export const CreditsSection: FC = () => {
+    return(
+        <div>
+            {/* TODO: implement */}
+        </div>
+    )
+}
