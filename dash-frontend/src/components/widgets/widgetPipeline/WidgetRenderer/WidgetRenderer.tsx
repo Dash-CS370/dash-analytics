0254637f2ca0e42d6c17426ade63d54e7098ffe1
Justin Lurie <jlurie02@gmail.com>
2024-03-21 11:20:29 -0400

    finished implementing widget renderer, witch routes graph info to the correct widget

--- dash-frontend/src/components/widgets/widgetPipeline/WidgetRenderer/WidgetRenderer.tsx
+++ dash-frontend/src/components/widgets/widgetPipeline/WidgetRenderer/WidgetRenderer.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { LineGraphWidget } from '../../graphWidgets/LineGraphWidget/LineGraphWidget';
+import { BaseGraphProps } from '../../WidgetTypes';
+import { BarChartWidget } from '../../graphWidgets/BarChartWidget/BarChartWidget';
 
-// TODO: add actual graph components
-const renderWidget = (graphType: string) => {
+const renderWidget = (graphType: string, graphInfo: BaseGraphProps) => {
     switch (graphType) {
-        case 'pie_chart':
-            return <div>pie chart</div>;
+        case 'bar_chart':
+            return (
+                <BarChartWidget title={graphInfo.title} data={graphInfo.data} />
+            );
         case 'line_graph':
-            return <div>line graph</div>;
+            return (
+                <LineGraphWidget
+                    title={graphInfo.title}
+                    data={graphInfo.data}
+                />
+            );
         default:
             throw new Error(`${graphType} is not a valid graph type`);
     }
@@ -14,10 +23,12 @@ const renderWidget = (graphType: string) => {
 
 interface WidgetRendererProps {
     graphType: string;
+    graphInfo: BaseGraphProps;
 }
 
 export const WidgetRenderer: React.FC<WidgetRendererProps> = ({
     graphType,
+    graphInfo,
 }) => {
-    return renderWidget(graphType);
+    return renderWidget(graphType, graphInfo);
 };
