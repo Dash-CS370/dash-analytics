283fd3be556bb8cdbb251e26285d167724e4ace3
Jason Lebov <jasonlebov@gmail.com>
2024-04-01 19:59:44 -0400

    Rename Column Functionality

--- dash-frontend/Data Pipeline/rename_col.tsx
+++ dash-frontend/Data Pipeline/rename_col.tsx
@@ -1,1 +1,13 @@
-// ADD RENAME FUNCTIONALITY - from Label Col Description Screen
+//  RENAME FUNCTIONALITY - from Label Column Description Screen (Only Called if User Renames A Feature)
+
+import * as dfd from 'danfojs';
+
+export async function rename_col(
+    original_col_name: string,
+    new_col_name: string,
+    dataframe: dfd.DataFrame,
+): Promise<dfd.DataFrame> {
+    // Renaming Columns
+    dataframe.rename({ original_col_name: new_col_name }, { inplace: true });
+    return dataframe;
+}
