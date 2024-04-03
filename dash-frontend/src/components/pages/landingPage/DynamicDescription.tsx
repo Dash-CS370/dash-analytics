f2828bd1d9f65005cc5f701ae5124d6118313ab0
Justin Lurie <jlurie02@gmail.com>
2024-04-03 12:46:57 -0400

    added running animation to dynamic description

--- dash-frontend/src/components/pages/landingPage/DynamicDescription.tsx
+++ dash-frontend/src/components/pages/landingPage/DynamicDescription.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import React, { useState, useEffect, FC } from 'react';
+import React, { useState, useEffect, FC, useRef } from 'react';
 import styles from '@/components/pages/landingPage/DynamicDescription.module.css';
+import Lottie from 'lottie-web';
+import animationData from '@/components/pages/landingPage/running.json';
 
 export const DynamicDescription: FC = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -11,6 +13,21 @@ export const DynamicDescription: FC = () => {
         'to precisely and swiftly analyze data',
         'to enhance team collaboration',
     ];
+
+    useEffect(() => {
+        let animationContainer = document.getElementById(
+            'animationContainer',
+        ) as HTMLDivElement;
+        if (animationContainer.childElementCount == 0) {
+            Lottie.loadAnimation({
+                container: animationContainer,
+                renderer: 'svg',
+                loop: true,
+                autoplay: true,
+                animationData: animationData,
+            });
+        }
+    }, []);
 
     useEffect(() => {
         const changeText = () => {
@@ -33,7 +50,17 @@ export const DynamicDescription: FC = () => {
             <span className={styles.constantDescription}>
                 Harness the power of Artificial Intelligence...
             </span>
-            <span className={styles.dynamicText}>{texts[currentIndex]}</span>{' '}
+            <div className={styles.dynamicContainer}>
+                <span className={styles.dynamicText}>
+                    {texts[currentIndex]}
+                </span>{' '}
+                <div className={styles.animationExpandingContainer}>
+                    <div
+                        id="animationContainer"
+                        className={styles.animationContainer}
+                    ></div>
+                </div>
+            </div>
             {/* Applying dynamic text */}
         </div>
     );
