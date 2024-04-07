cf7f07ab962a4cbb9c0bd2ac9e4cb50d92ef01ee
Justin Lurie <jlurie02@gmail.com>
2024-04-06 23:17:38 -0400

    local signin

--- dash-frontend/src/app/signin/page.tsx
+++ dash-frontend/src/app/signin/page.tsx
@@ -118,7 +118,7 @@ export default function Signin() {
                         <div className={styles.externalAccountOpts}>
                             <SigninButton
                                 buttonText="Login with"
-                                href="http://3.138.112.56:8080/oauth2/authorization/google"
+                                href="http://127.0.0.1:8080/oauth2/authorization/google"
                                 imgSrc="assets/google.svg"
                                 alt="Google Logo"
                                 imgWidth={35}
