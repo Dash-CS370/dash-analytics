2a56db3001acfe37a0121f5393865f32050f269a
tcmcgarry <timothymcgarry1@gmail.com>
2024-04-16 20:34:50 -0400

    changed smoothing scaling function

--- dash-frontend/src/components/dataPipeline/dataOperations/sliceData.tsx
+++ dash-frontend/src/components/dataPipeline/dataOperations/sliceData.tsx
@@ -1,4 +1,6 @@
 import { DataFrame } from 'danfojs';
+import * as dfd from 'danfojs';
+import {logarithmicScaling} from "@/components/dataPipeline/dataOperations/operations";
 
 async function determineRowCount(df: DataFrame): Promise<number> {
     return Promise.resolve(df.shape[0]);
@@ -9,9 +11,10 @@ async function calculateSliceInterval(rowCount: number, threshold: number, minIn
         return minInterval;
     } else {
         // scaling with square root
-        let scalingFactor = Math.sqrt(rowCount - threshold);
-        return Math.max(minInterval, scalingFactor);
-
+        // let scalingFactor = Math.sqrt(rowCount - threshold);
+        let scalingFactor = Math.ceil(rowCount / threshold);
+        // return Math.max(minInterval, scalingFactor);
+        return scalingFactor;
         // // linear scaling constant
         // const scalingConstant = 0.01;
         // let scalingFactor = scalingConstant * (rowCount - threshold);
@@ -35,6 +38,7 @@ async function sliceDF(df: DataFrame, n: number): Promise<DataFrame> {
             indices.push(i);
         }
         const slicedDF = df.iloc({ rows: indices });
+        slicedDF.print();
         resolve(slicedDF);
     });
 }
@@ -57,20 +61,29 @@ export async function processAndSliceDF(df: DataFrame, threshold: number, minInt
     }
 }
 
-const testData = {
-    "column1": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    "column2": [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-  };
+dfd.readCSV("csvData/newcsv.csv").then((df) => {
+            processAndSliceDF(df, 3000, 1).then((slicedDF) => {
+                console.log('sliced df:');
+                slicedDF.print();
+            });
+            console.log(df);
+        });
 
-const df = new DataFrame(testData);
-
-const inputCsvPath = 'csvData/500000sales.csv'; // Update this path to your input CSV file
-const threshold = 10000;
-const minInterval = 1;
-
-processAndSliceDF(df, threshold, minInterval)
-    .then(slicedDF => {
-        console.log("DataFrame after processing and slicing:");
-        slicedDF.print();
-    })
-    .catch(console.error);
+//
+// const testData = {
+//     "column1": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+//     "column2": [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+//   };
+//
+// const df = new DataFrame(testData);
+//
+// const inputCsvPath = 'csvData/500000sales.csv'; // Update this path to your input CSV file
+// const threshold = 8000;
+// const minInterval = 1;
+//
+// processAndSliceDF(df, threshold, minInterval)
+//     .then(slicedDF => {
+//         console.log("DataFrame after processing and slicing:");
+//         slicedDF.print();
+//     })
+//     .catch(console.error);
