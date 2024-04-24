16766bc11f9a60f2e71a5abedae1ec9b48e65d2f
gpmorales <georgepm20002@gmail.com>
2024-04-24 19:44:33 -0400

    Bar graph renderer under construction

---
+++ dash-frontend/src/components/dataPipeline/dataOperations/BarGraphRenderer.tsx
@@ -0,0 +1,8 @@
+import * as dfd from 'danfojs';
+
+
+
+// When a Bar Graph widget generated is to be this is called to prepare the
+export function generateBarChart(dataframe_column: dfd.DataFrame): any {
+
+}
