ce36e06e0d88727b4383bea359c20abc01af2743
gpmorales <georgepm20002@gmail.com>
2024-04-24 04:00:30 -0400

    Stats card dataops tested and impl

--- dash-frontend/src/components/dataPipeline/dataOperations/statsCardPipeline.tsx
+++ dash-frontend/src/components/dataPipeline/dataOperations/statsCardPipeline.tsx
@@ -1,31 +1,25 @@
 import * as dfd from 'danfojs';
 
 export function generateStatsCard(dataframe_column: dfd.Series): any {
-    return new Promise((resolve, reject) => {
+    // Categorical column
+    if (dataframe_column.dtype == 'string') {
         try {
-            if (dataframe_column.dtype == 'string') { // if non-numerical values (string)
-                try {
-                    analyzeCategoricalColumn(dataframe_column);
-                } catch (error) {
-                    console.error("Error calculating mean price:", error);
-                }
-            } else {
-                try {
-                    analyzeNumericalColumn(dataframe_column);
-                } catch (error) {
-                    console.error("Error calculating:", error)
-                }
-            }
-
+            return analyzeCategoricalColumn(dataframe_column);
         } catch (error) {
-            console.log(error)
+            console.error("Error calculating mean price:", error);
         }
-    });
+    } else { // Numerical column
+        try {
+            return analyzeNumericalColumn(dataframe_column);
+        } catch (error) {
+            console.error("Error calculating:", error)
+        }
+    }
 }
 
 
 // takes in Series of categorical data, outputs dictionary of percentages of whole
-export async function analyzeCategoricalColumn(categorical_column: dfd.Series) {
+function analyzeCategoricalColumn(categorical_column: dfd.Series) {
 
     const totalCount = categorical_column.size;
     const categoryCounts = categorical_column.valueCounts();
@@ -40,37 +34,34 @@ export async function analyzeCategoricalColumn(categorical_column: dfd.Series) {
 
     let sortedCategoriesAndStats = categoriesAndStats.sort((a, b) => b[1] - a[1]);
 
-    let categoricalStats = sortedCategoriesAndStats.map((pair) => {
+    return sortedCategoriesAndStats.map((pair) => {
         return [pair[0], `${pair[1]}%`]
     })
-
-    console.log("Sorted Categories and Percentages:", categoricalStats);
 }
 
 
 
 
 // takes in Series of numerical data, outputs dictionary of stats
-export async function analyzeNumericalColumn(numerical_column: dfd.Series) {
+function analyzeNumericalColumn(numerical_column: dfd.Series) {
     // Sort the numerical column to calculate quartiles
     const sortedValues = numerical_column.sortValues({ ascending: true }).values;
 
     const mean = numerical_column.mean();
     const median = numerical_column.median();
-    const mode = numerical_column.mode();
     const min = numerical_column.min();
     const max = numerical_column.max();
 
     // Calculate quartiles
+    /*
     const q1 = Number(sortedValues[Math.floor(sortedValues.length / 4)]); // First quartile
     const q3 = Number(sortedValues[Math.ceil((sortedValues.length * 3) / 4)]); // Third quartile
-
     const IQR : number = q3 - q1; // Interquartile range
     const lowerBound = q1 - 1.5 * IQR; // Lower bound for outliers
     const upperBound = q3 + 1.5 * IQR; // Upper bound for outliers
-
     // @ts-ignore
     const outliers = sortedValues.filter((v: number) => v < lowerBound || v > upperBound);
+    */
 
     return {
         mean, //number
@@ -80,3 +71,4 @@ export async function analyzeNumericalColumn(numerical_column: dfd.Series) {
     };
 
 }
+
